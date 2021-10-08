c2db82a95daab2fafa5c1949fcdbca2fed7d9d80
Ronan Darcy <durd@ecgb11.ecmwf.int>
2021-10-08 13:47:33 +0000

    Updated scripts for better consistency Better support on ecgate/cca

--- bin/create_suite.py
+++ bin/create_suite.py
@@ -9,25 +9,34 @@ import argparse
 
 import ecflow as ec
 
-EXP = os.environ['EXP']
+# The name of this suite
+SUITE_NAME = os.environ['SUITE_NAME']
 
+# User/host variables
 CLUSTER = os.environ['HOSTNAME']
 USER = os.environ["USER"]
 
+# Ecflow variables
 ECF_HOST = os.environ["ECF_HOST"]
 ECF_PORT = os.environ["ECF_PORT"]
 ECF_HOME = os.environ["ECF_HOME"]
+
+# Where this project is installed
 EXTRGRIB = os.environ["EXTRGRIB"]
 
 # Force replace suite
 FORCE = os.environ["FORCE"]
 
+# Recover the bash variable determining which models to process
+MODEL_SUITES = os.environ["MODEL_SUITES"]
+# Convert this variable to a python list
+model_suites = MODEL_SUITES.split(" ")
 
 
 defs = ec.Defs()
-suite = defs.add_suite(EXP)
+suite = defs.add_suite(SUITE_NAME)
 suite.add_variable("USER",           USER)
-suite.add_variable("EXP",            EXP)
+suite.add_variable("SUITE_NAME",     SUITE_NAME)
 suite.add_variable("ECF_HOME",       "%s"%ECF_HOME)
 suite.add_variable("ECF_INCLUDE",    "%s/etc/ecf"%EXTRGRIB)
 suite.add_variable("ECF_FILES",      "%s/etc/ecf"%EXTRGRIB)
@@ -52,24 +61,104 @@ if "ecgb" in CLUSTER:
     suite.add_variable("ECF_JOB_CMD",       ECF_JOB_CMD)
     suite.add_variable("ECF_KILL_CMD",      ECF_KILL_CMD)
     suite.add_variable("ECF_STATUS_CMD",    ECF_STATUS_CMD)
-    suite.add_variable("ECF_HOME",          "/hpc%s"%LBC_WORK)
-    suite.add_variable("ECF_INCLUDE",       "/hpc%s/share/ecf"%HARMLBCS)
-    suite.add_variable("ECF_FILES",         "/hpc%s/share/ecf"%HARMLBCS)
+    suite.add_variable("ECF_HOME",          "/hpc%s"%ECF_HOME)
+    suite.add_variable("ECF_INCLUDE",       "/hpc%s/share/ecf"%EXTRGRIB)
+    suite.add_variable("ECF_FILES",         "/hpc%s/share/ecf"%EXTRGRIB)
     suite.add_variable("SUB_H",             "qsub.h")
 
+
+
+
+
+# The run family should contain the operational functionality of the suite
+# If a task in run aborts, operators will be instructed to intervene
 def create_family_run():
+    #### These variables should be sourced from HARMONIE expirement directory ###
+    # Source HM_LIB/ecf/config_mbr${ENSMBR}.h 
+    start_ymd = "20211001" # From progress.log
+    #############################################################################
+
+
     run = ec.Family("run")
     run.add_limit("par", 6)
+    run.add_repeat(ec.RepeatDate("YMD",int(start_ymd), 20990101, 1))
+
+
+    # For this experiment, recover the list of cycles HH_LIST
+    HH_LIST = os.environ["HH_LIST"]
+    # Split this (e.g. 00-18:6) into the range (e.g. 00-18) and the steps (e.g. 6)
+    HH_LIST_range, HH_LIST_steps = HH_LIST.split(':')
+    # Construct a list object consisting of each valid cycle
+    hh_list = sum(((list(range(*[int(b) + c
+        for c, b in enumerate(a.split('-'))], int(HH_LIST_steps)))
+        if '-' in a else [int(a)]) for a in HH_LIST_range.split(',')), [])
+    # Pad this list to have leading zeroes, i.e. 0 -> 00 etc
+    cycle_list = [str(cycle).zfill(2) for cycle in hh_list]
+
+    cycle_i = 0
+    for cycle in cycle_list:
+        fc = run.add_family(cycle).add_inlimit("par")
+        fc.add_variable("HH", cycle)
+        null="null"
+        # For this experiment, recover the list of ensemble members ENSMSEL
+        if ENSMSEL == "":
+            print("Deterministic forecast only")
+            ensmbrs = [ 'det' ]
+        else:
+            # Convert this string into a list item containing all ensemble members
+            ensmsel = sum(((list(range(*[int(b) + c
+                for c, b in enumerate(a.split('-'))]))
+                if '-' in a else [int(a)]) for a in ENSMSEL.split(',')), [])
+            # Pad this list to 3 digits
+            ensmbrs = [str(mbr).zfill(3) for mbr in ensmsel]
+
+        # Iterate over each ensemble member
+        for mbr in ensmbrs:
+            # Retrieve the LL_LIST for this member
+            LL_LIST = os.environ[model_suite + "_mbr" + mbr + "_LL_LIST"]
+            LL_LIST = LL_LIST.split(",")
+
+            # LL_LIST determines the LL for this particular member and cycle
+            # Expand the LL_LIST to the length of cycle_list
+            q, r = divmod(len(cycle_list), len(LL_LIST))
+            ll_list = q * LL_LIST + LL_LIST[:r]
+            # Get the ll for this member/cycle using the cycle_i iterator
+            max_ll = ll_list[cycle_i]
+
+            fm = fc.add_family("mbr"+mbr)
+            # Update variables required for extractGrib
+            fm.add_variable("FCSTPATH",     null)
+            fm.add_variable("EXPT",         null)
+            fm.add_variable("DOMAIN",       DOMAIN)
+            fm.add_variable("EZONE",        null)
+            fm.add_variable("DTG",          null)
+            fm.add_variable("STEP",         null)
+            fm.add_variable("ENSMBR",       mbr)
+            fm.add_variable("MAX_LL",       max_ll)
+            fm.add_task("extract_grib")
+
+        # Iterate cycle_i so we know which ll to pick from ll_list
+        cycle_i = cycle_i + 1
     return run
 
+# The maintenance family should contain housekeeping tasks such as deleting
+# old files, cleaning working directories, archiving etc.
+# If a task in maint aborts, operators will issue a next working day request
 def create_family_maint():
     maint= ec.Family("maint")
     maint.add_limit("par", 6)
     return maint
 
 # Create the families in the suite
-suite.add_family(create_family_run())
-suite.add_family(create_family_maint())
+for model_suite in model_suites:
+    fm = suite.add_family(model_suite)
+    # Recover ensemble list ENSMSEL
+    ENSMSEL = os.environ[model_suite + '_ENSMSEL']
+    DOMAIN = os.environ[model_suite + '_DOMAIN']
+
+    fm.add_variable("EXPT",     model_suite)
+    fm.add_family(create_family_run())
+    fm.add_family(create_family_maint())
 
 # Define a client object with the target ecFlow server
 client = ec.Client(ECF_HOST, ECF_PORT)
@@ -88,8 +177,8 @@ else:
         exit(1)
 
 # Save the definition to a .def file
-print("Saving definition to file '%s.def'"%EXP)
-defs.save_as_defs("%s.def"%EXP)
+print("Saving definition to file '%s.def'"%SUITE_NAME)
+defs.save_as_defs("%s.def"%SUITE_NAME)
 exit(0)
 print("loading on %s@%s" %(ECF_HOST,ECF_PORT))
 
@@ -114,3 +203,4 @@ client.resume("/%s" %suite.name())
 
 
 exit(0)
+
--- bin/create_suite.sh
+++ bin/create_suite.sh
@@ -7,17 +7,53 @@
 # by the installation script (cmake)              #
 # # # # # # # # # # # # # # # # # # # # # # # # # #
 
-module purge
-module load eccodes/gnu-4.8.5/2.18.0
-module load ecflow/gnu-7.3.1/5.5.3
+#module purge
+#module load eccodes/gnu-4.8.5/2.18.0
+#module load ecflow/gnu-7.3.1/5.5.3
+#source ../share/config/config.ecgb-cca
+#set -x
+export SUITE_NAME="makegrib"
 
-export EXP="makegrib"
-
-export ECF_HOST="reaserve"
-export ECF_PORT="5518"
+#export ECF_HOST="reaserve"
+#export ECF_PORT="5518"
 export FORCE="True"
 
-export EXTRGRIB="/data/rdarcy/uwcwtest_repos/extractgrib"
-export ECF_HOME="/home/rdarcy/ecflow_suites"
+export EXTRGRIB="$PERM/uwcwtest_repos/extractGrib"
+export ECF_HOME="$HOME/ecflow_suites"
+
+source $EXTRGRIB/share/config/config.ecgb-cca
+# Source config_exp
+
+for model_suite in $MODEL_SUITES
+do
+    # Source the Env_system file to determine HM_LIB
+    export EXP=$model_suite
+    source $HOME/hm_home/${model_suite}/Env_system
+    echo "HM_LIB=$HM_LIB"
+    #HM_LIB="/data/rdarcy/uwcwtest_repos/extractgrib/etc/config"
+    source ${HM_LIB}/ecf/config_exp.h || exit 1
+    # Get member information
+    if [ -z $ENSMSEL ]
+    then
+        export "${model_suite}_mbrdet_ENSMSEL="$ENSMSEL""
+        export "${model_suite}_mbrdet_LL_LIST="$LL_LIST""
+    else
+        # Get the list of ensemble members
+        module unload python
+        module load python3/3.8.8-01
+
+        ENSMBRS=$(python3 ensmsel_to_list.py $ENSMSEL)
+        ENSMBRS=$(echo $ENSMBRS | sed -E "s/\[|\]|'|,//g")
+        for ENSMBR in $ENSMBRS
+        do
+            source ${HM_LIB}/ecf/config_mbr${ENSMBR}.h || exit 1
+            export "${model_suite}_mbr${ENSMBR}_LL_LIST="$LL_LIST""
+        done
+
+    fi
+    export "${model_suite}_ENSMSEL="$ENSMSEL""
+    export "${model_suite}_DOMAIN="$DOMAIN""
+    source $EXTRGRIB/share/config/config.ecgb-cca
+done
 
 python3 create_suite.py

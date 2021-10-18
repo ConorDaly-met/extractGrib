f938c40b0afde8178e6019104d055f69a2136e84
Conor Daly <conor.daly@met.ie>
2021-10-18 15:17:33 +0000

    Conor Daly: Increased EC_memory_per_task to 70GB

--- share/ecf/qsub.h
+++ share/ecf/qsub.h
@@ -1,7 +1,7 @@
 #PBS -q nf
 #PBS -l EC_total_tasks=1
 #PBS -l EC_tasks_per_node=1
-#PBS -l EC_memory_per_task=30000MB 
+#PBS -l EC_memory_per_task=70000MB 
 #PBS -A uwcwecds
 #PBS -v OMP_NUM_THREADS=12
 #PBS -l EC_threads_per_task=12
